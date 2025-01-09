821491176e0fbabb6f568c87ff9aa6dc99265119
aodroiet <aodroiet@outlook.com>
2025-01-09 21:47:00 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,33 @@
 #include "html.h"
+#include <EEPROM.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
 #include <ModbusMaster.h>
 #include <SoftwareSerial.h>
 
-#define RX_PIN 12
-#define TX_PIN 13
-#define RTS_PIN 14
+#define MAX485_RO_PIN 13    // RX
+#define MAX485_DI_PIN 14    // TX
+#define MAX485_DE_RE_PIN 12 // DE/RE
 
-const long fetchInterval = 1000;
+#define LED_PIN 2
+#define RESET_PIN 0
 
-const char *ssid = "ssid";
-const char *password = "password";
+#define FETCH_INTERVAL 1000
+#define REQUEST_TIMEOUT 10000
+#define RESET_HOLD_TIME 5000
 
+String ap_ssid;
 ModbusMaster node;
 ESP8266WebServer WebServer(80);
-SoftwareSerial RS485Serial(RX_PIN, TX_PIN);
+SoftwareSerial RS485(MAX485_RO_PIN, MAX485_DI_PIN);
+
+bool isFetching = false;
+bool resetPending = false;
+bool dataRequested = false;
+bool wifiConfigured = false;
+
+unsigned long lastFetchTime = 0;
+unsigned long lastRequestTime = 0;
 
 struct SensorData {
   uint16_t Inverter_State;
@@ -36,23 +48,17 @@ struct SensorData {
 } sensorData;
 
 void preTransmission() {
-  digitalWrite(RTS_PIN, HIGH);
+  digitalWrite(MAX485_DE_RE_PIN, HIGH);
 }
 
 void postTransmission() {
-  digitalWrite(RTS_PIN, LOW);
+  digitalWrite(MAX485_DE_RE_PIN, LOW);
 }
 
 bool fetchData() {
-  static unsigned long lastFetchTime = 0;
-  if (millis() - lastFetchTime < fetchInterval) {
-    return false;
-  }
-
   uint8_t result = node.readHoldingRegisters(0x0000, 0x1F);
-  if (result != node.ku8MBSuccess) {
+  if (result != node.ku8MBSuccess)
     return false;
-  }
 
   sensorData.Inverter_State = node.getResponseBuffer(0x00);
   sensorData.PV_Voltage = node.getResponseBuffer(0x06) * 0.1;
@@ -70,60 +76,196 @@ bool fetchData() {
   sensorData.Temperature_Inverter = node.getResponseBuffer(0x1C);
   sensorData.Total_Running_Hour = (node.getResponseBuffer(0x17) << 16) + node.getResponseBuffer(0x18);
 
-  lastFetchTime = millis();
   return true;
+}
+
+void blinkLED(int pin, int times, int onTime, int offTime) {
+  for (int i = 0; i < times; ++i) {
+    digitalWrite(pin, LOW);
+    delay(onTime);
+    digitalWrite(pin, HIGH);
+    delay(offTime);
+  }
+}
+
+void checkButton() {
+  static unsigned long btnStart = 0;
+  if (digitalRead(RESET_PIN) == LOW) {
+    if (btnStart == 0) {
+      btnStart = millis();
+    } else if (millis() - btnStart >= RESET_HOLD_TIME && !resetPending) {
+      resetPending = true;
+      for (int i = 0; i < 128; ++i) {
+        EEPROM.write(i, 0);
+      }
+      EEPROM.commit();
+      wifiConfigured = false;
+      Serial.println("\nWiFi settings reset! Rebooting...");
+      blinkLED(LED_PIN, 5, 500, 500);
+      ESP.restart();
+    }
+  } else {
+    btnStart = 0;
+    resetPending = false;
+  }
+}
+
+String trimString(const String &str) {
+  int start = 0;
+  int end = str.length() - 1;
+  while (start <= end && isspace(str[start]))
+    start++;
+  while (end >= start && isspace(str[end]))
+    end--;
+  return str.substring(start, end + 1);
+}
+
+void saveWiFiConfig() {
+  String ssid = trimString(WebServer.arg("ssid"));
+  String password = trimString(WebServer.arg("password"));
+  String ipconfig = WebServer.arg("ipconfig");
+  for (int i = 0; i < 32; ++i) {
+    EEPROM.write(i, ssid[i]);
+    EEPROM.write(32 + i, password[i]);
+  }
+  EEPROM.write(64, ipconfig == "static" ? 1 : 0);
+  if (ipconfig == "static") {
+    String address = trimString(WebServer.arg("address"));
+    String gateway = trimString(WebServer.arg("gateway"));
+    String netmask = trimString(WebServer.arg("netmask"));
+    for (int i = 0; i < 32; ++i) {
+      EEPROM.write(65 + i, address[i]);
+      EEPROM.write(97 + i, gateway[i]);
+      EEPROM.write(129 + i, netmask[i]);
+    }
+  }
+  EEPROM.commit();
+  wifiConfigured = true;
+  WebServer.send(200, "text/html", "<h1>WiFi settings saved! Rebooting...</h1>");
+  delay(1000);
+  ESP.restart();
+}
+
+String readEEPROMString(int startAddr, int length) {
+  String str;
+  for (int i = 0; i < length; ++i) {
+    char c = EEPROM.read(startAddr + i);
+    if (c != 0)
+      str += c;
+  }
+  return str;
+}
+
+void connectToWiFi() {
+  String ssid = readEEPROMString(0, 32);
+  String password = readEEPROMString(32, 32);
+  bool isStaticIP = EEPROM.read(64);
+  if (ssid.length() > 0) {
+    if (isStaticIP) {
+      IPAddress address, gateway, netmask;
+      address.fromString(readEEPROMString(65, 32));
+      gateway.fromString(readEEPROMString(97, 32));
+      netmask.fromString(readEEPROMString(129, 32));
+      WiFi.config(address, gateway, netmask);
+    }
+    if (password.length() > 0) {
+      WiFi.begin(ssid.c_str(), password.c_str());
+    } else {
+      WiFi.begin(ssid.c_str());
+    }
+    while (WiFi.status() != WL_CONNECTED) {
+      blinkLED(LED_PIN, 1, 20, 1000);
+      Serial.print(".");
+      checkButton();
+    }
+    Serial.println("\nConnected to " + ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+    wifiConfigured = true;
+  } else {
+    WiFi.softAP(ap_ssid);
+    Serial.println("\nAccess Point started!");
+  }
 }
 
 void setup() {
   Serial.begin(115200);
 
-  RS485Serial.begin(9600);
-  node.begin(0x01, RS485Serial);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
+
+  pinMode(RESET_PIN, INPUT_PULLUP);
+
+  pinMode(MAX485_DE_RE_PIN, OUTPUT);
+  digitalWrite(MAX485_DE_RE_PIN, LOW);
+
+  EEPROM.begin(512);
+  connectToWiFi();
+
+  RS485.begin(9600);
+  node.begin(0x01, RS485);
   node.preTransmission(preTransmission);
   node.postTransmission(postTransmission);
 
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.print("\nConnected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
   WebServer.on("/", HTTP_GET, []() {
-    WebServer.send(200, "text/html", MAIN_page);
+    String html = wifiConfigured ? MAIN_page : WiFi_page;
+    html.replace("%CSS_STYLES%", CSS_styles);
+    WebServer.send(200, "text/html", html);
   });
 
   WebServer.on("/data", HTTP_GET, []() {
-    String jsonResponse = "{";
-    jsonResponse += "\"Inverter_State\":" + String(sensorData.Inverter_State) + ",";
-    jsonResponse += "\"PV_Voltage\":" + String(sensorData.PV_Voltage) + ",";
-    jsonResponse += "\"PV_Current\":" + String(sensorData.PV_Current) + ",";
-    jsonResponse += "\"PV_Power\":" + String(sensorData.PV_Power) + ",";
-    jsonResponse += "\"Bus_Voltage\":" + String(sensorData.Bus_Voltage) + ",";
-    jsonResponse += "\"AC_Voltage\":" + String(sensorData.AC_Voltage) + ",";
-    jsonResponse += "\"AC_Current\":" + String(sensorData.AC_Current) + ",";
-    jsonResponse += "\"Grid_Frequency\":" + String(sensorData.Grid_Frequency) + ",";
-    jsonResponse += "\"Active_Power\":" + String(sensorData.Active_Power) + ",";
-    jsonResponse += "\"Reactive_Power\":" + String(sensorData.Reactive_Power) + ",";
-    jsonResponse += "\"Daily_Production\":" + String(sensorData.Daily_Production) + ",";
-    jsonResponse += "\"Total_Production\":" + String(sensorData.Total_Production) + ",";
-    jsonResponse += "\"Temperature_Module\":" + String(sensorData.Temperature_Module) + ",";
-    jsonResponse += "\"Temperature_Inverter\":" + String(sensorData.Temperature_Inverter) + ",";
-    jsonResponse += "\"Total_Running_Hour\":" + String(sensorData.Total_Running_Hour) + ",";
-    jsonResponse += "\"RSSI\":" + String(WiFi.RSSI());
-    jsonResponse += "}";
-    WebServer.send(200, "application/json", jsonResponse);
-    // WebServer.sendHeader("Access-Control-Allow-Origin", "*");
+    WebServer.sendHeader("Access-Control-Allow-Origin", "*");
+
+    dataRequested = true;
+    lastRequestTime = millis();
+
+    String json = "{";
+    json += "\"Inverter_State\":" + String(sensorData.Inverter_State) + ",";
+    json += "\"PV_Voltage\":" + String(sensorData.PV_Voltage) + ",";
+    json += "\"PV_Current\":" + String(sensorData.PV_Current) + ",";
+    json += "\"PV_Power\":" + String(sensorData.PV_Power) + ",";
+    json += "\"Bus_Voltage\":" + String(sensorData.Bus_Voltage) + ",";
+    json += "\"AC_Voltage\":" + String(sensorData.AC_Voltage) + ",";
+    json += "\"AC_Current\":" + String(sensorData.AC_Current) + ",";
+    json += "\"Grid_Frequency\":" + String(sensorData.Grid_Frequency) + ",";
+    json += "\"Active_Power\":" + String(sensorData.Active_Power) + ",";
+    json += "\"Reactive_Power\":" + String(sensorData.Reactive_Power) + ",";
+    json += "\"Daily_Production\":" + String(sensorData.Daily_Production) + ",";
+    json += "\"Total_Production\":" + String(sensorData.Total_Production) + ",";
+    json += "\"Temperature_Module\":" + String(sensorData.Temperature_Module) + ",";
+    json += "\"Temperature_Inverter\":" + String(sensorData.Temperature_Inverter) + ",";
+    json += "\"Total_Running_Hour\":" + String(sensorData.Total_Running_Hour) + ",";
+    json += "\"RSSI\":" + String(WiFi.RSSI());
+    json += "}";
+
+    WebServer.send(200, "application/json", json);
   });
 
+  WebServer.on("/save", HTTP_POST, saveWiFiConfig);
   WebServer.begin();
 }
 
 void loop() {
   WebServer.handleClient();
-  fetchData();
+
+  if (WiFi.status() != WL_CONNECTED) {
+    blinkLED(LED_PIN, 1, 20, 3000);
+  }
+
+  if (dataRequested && millis() - lastFetchTime >= FETCH_INTERVAL) {
+    if (!isFetching) {
+      Serial.println("Starting data fetching...");
+      isFetching = true;
+    }
+    fetchData();
+    lastFetchTime = millis();
+  }
+
+  if (millis() - lastRequestTime > REQUEST_TIMEOUT && dataRequested) {
+    Serial.println("Stopping data fetching...");
+    dataRequested = false;
+    isFetching = false;
+  }
+
+  checkButton();
 }
